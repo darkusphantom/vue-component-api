800d8993dbb161d28fa6170b724112ac0e6cc0b5
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-08-08 10:43:13 -0400

    componentes asincronos

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineAsyncComponent } from 'vue';
+
+const HelloWorld = defineAsyncComponent(() => import('./components/HelloWorld.vue'));
 
 export default {
   name: 'App',
