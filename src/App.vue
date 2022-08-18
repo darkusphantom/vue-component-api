f7b7b75e309ccf3fd3c258353770dba8b2767a1f
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-08-18 15:28:46 -0400

    ciclos de vida con pokeapi

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <template>
-  <p>{text}</p>
+  <p>Pokemon 1: {{text}}</p>
+  <p>Pokemon 2: {{ text2 }} </p>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   components: {  },
-  data() {
-    return {
-      text: "Zangana"
+  data: () => ({
+    text: "Zangana",
+    text2: "Dorime",
+  }),
+  methods: {
+    async getPokemon() {
+      const URL = 'https://pokeapi.co/api/v2/pokemon/255';
+      const response = await fetch(URL);
+      const data = await response.json();
+
+      const URL2 = 'https://pokeapi.co/api/v2/pokemon/404';
+      const response2 = await fetch(URL2);
+      const data2 = await response2.json();
+
+      this.text = data.name;
+      this.text2 = data2.name;
     }
   },
   beforeCreate() {
     console.log("Before create", this.$data, this.$el);
+
   },
   created() {
     console.log("created", this.$data, this.$el);
+    setTimeout(() => this.getPokemon(), 3000);
   },
   mounted() {
     console.log("Mounted", this.$data, this.$el);
